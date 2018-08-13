792f63025a63c4f2c0a7a248512a169b857053a3
Tiago Santos <santos.tiago@gmail.com>
2018-08-13 17:50:26 +0100

    Changed the import script to split insertions when its size is too big

--- database_creation.rb
+++ database_creation.rb
@@ -5,9 +5,10 @@ require 'ruby-progressbar'
 
 conn = PGconn.connect(host: "localhost", port: 5432, user: 'postgres', dbname: 'flood')
 
-large_files = %w[Raw_agg_Coastal_City_POPexp Raw_agg_Coastal_City_GDPexp]
+large_files = %w[Raw_agg_Coastal_City_POPexp]
+MAX_COLUMNS = 1000
 
-Dir['floods_vizzuality/Flood_Data/Raw_agg_Coastal_City_GDPexp.csv'].each do |f|
+Dir['floods_vizzuality/Flood_Data/*.csv'].each do |f|
   # Gets table name from filename
   tablename = f[/[^\/]+(?=\.csv)/, 0]
   puts "Starting with file #{tablename}"
@@ -22,7 +23,7 @@ Dir['floods_vizzuality/Flood_Data/Raw_agg_Coastal_City_GDPexp.csv'].each do |f|
   # Gets the columns names
   file = File.open(f)
   columns = CSV.parse(file.first).first
-  columns.map! { |x| x.nil? ? 'id' : x}
+  columns.map! { |x| x.nil? ? 'id' : x }
 
   # Information for the progress bar
   number_of_lines = file.readlines.size / 100
@@ -48,31 +49,47 @@ Dir['floods_vizzuality/Flood_Data/Raw_agg_Coastal_City_GDPexp.csv'].each do |f|
   progress_bar = ProgressBar.create(title: tablename)
 
   line = 0
-  total = 0
   CSV.foreach(f, col_sep: ',', row_sep: :auto, headers: true, encoding: 'UTF-8') do |row|
     line += 1
-    total += 1
     if line > number_of_lines
       progress_bar.increment
       line = 0
     end
-    data = row.to_h
-    insert_query = "INSERT INTO #{tablename} ("
-    columns = []
-    values = []
-    data.each do |d|
-      next if d.first.nil? || d.last.nil?
-      columns << d.first
-      values << (string_columns.include?(d.first) ? "'#{d.last.gsub("'", "''")}'" : d.last)
-    end
+    data = row.to_a
+    # Most files have the id field without name, so this hack fixes it
+    data[0][0] = 'id' if data[0][0].nil?
 
-    insert_query << columns.join(', ')
-    insert_query << ') VALUES ( ' + values.join(', ') + ');'
+    column_number = 0
+    row_id = nil
+    loop do
+      columns = []
+      values = []
 
-    begin
-      conn.exec(insert_query)
-    rescue
-      puts insert_query
+      data[column_number...(column_number + MAX_COLUMNS)].each do |d|
+        next if d.first.nil? || d.last.nil?
+        columns << d.first
+        values << (string_columns.include?(d.first) ? "'#{d.last.gsub("'", "''")}'" : d.last)
+      end
+
+      if column_number.zero?
+        insert_query = "INSERT INTO #{tablename} ("
+        insert_query << columns.join(', ')
+        insert_query << ') VALUES ( ' + values.join(', ') + ') RETURNING id;'
+      else
+        col_val = columns.each_with_index.map { |e, i| " #{e} = #{values[i]}" }
+        insert_query = "UPDATE #{tablename} SET"
+        insert_query << col_val.join(', ')
+        insert_query << " WHERE id = '#{row_id}' RETURNING id;"
+      end
+
+      begin
+        response = conn.exec(insert_query)
+        row_id = response[0]['id'] if response[0] && response[0]['id']
+      rescue
+        puts insert_query
+      end
+      column_number += MAX_COLUMNS
+      break if column_number > data.length
     end
   end
   progress_bar.finish
